014721b367b96d5aa92f372d8664a5b8524c1c8f
Håvard Slettvold <havardsv@stud.ntnu.no>
2012-03-05 18:16:40 +0100

    More updates.

--- eventsystem/events/urls.py
+++ eventsystem/events/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('eventsystem.events.views',
     url(r'^(?P<event_id>\d+)/$', 'details', name='details'),
     url(r'^(?P<event_id>\d+)/attend/$', 'attend', name='attend'),
     url(r'^(?P<event_id>\d+)/unattend/$', 'unattend', name='unattend'),
-    url(r'^(?P<event_id>\d+)/attendees/$', pdf, name='event_attendees'),
+    #url(r'^(?P<event_id>\d+)/attendees/$', pdf, name='event_attendees'),
 )
